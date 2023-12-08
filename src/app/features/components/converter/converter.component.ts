692da269640479f87d2fd18f9873f94c3c4abdec
AhmedAbdelfattah0 <ahmed.m.abdelfttah@gmail.com>
2023-12-08 12:02:37 +0200

    feat(converter): Add currency conversion functionality and UI
    This commit adds the necessary code to implement currency conversion
    functionality and the corresponding UI in the converter component. The
    changes include:
    - New HTML template for the converter component with form inputs for
    amount, from currency, to currency, and conversion result display.
    - New SCSS file for styling the converter component.
    - New TypeScript file that defines the ConverterComponent class and
    handles currency conversion logic using the ApiService.
    - Import of necessary dependencies such as FormGroup, FormControl,
    Validators, FormBuilder, and ApiService.
    - Implementation of the ngOnInit lifecycle hook and the getSymbols and
    convertCurrency methods.
    - Testing of the ConverterComponent class with a simple unit test.
    
    These changes provide users with the ability to input an amount, select
    the from and to currencies, and convert the currency using an external
    API. The conversion result is then displayed to the user. This
    functionality improves the usability and usefulness of the converter
    component.
    
    No issues are associated with this commit.

---
+++ src/app/features/components/converter/converter.component.ts
@@ -0,0 +1,73 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from './../../../shared/services/api.service';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/base-components/base.component';
+import { takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent extends BaseComponent implements OnInit {
+  exchangeData: any;
+  calculatedConversion: any;
+  amount: any = 1;
+  storeAmount: any;
+  exchangeRates: any;
+  conversion_result: any = '';
+  conversion_rate: any = '1.9';
+  base_code: any = 'EUR';
+  target_code: any = 'USD';
+  conversionResult: any;
+  symbols: any;
+  fromSelectedCurrency: string = 'EUR';
+  toSelectedCurrency: string = 'USD';
+  constructor(private formBuilder: FormBuilder, private apiService:ApiService) {
+    super();
+    this.getSymbols()
+
+  }
+  conversionForm = new FormGroup({
+    amount: new FormControl('', [Validators.required]),
+    fromCurrency: new FormControl('', [Validators.required]),
+    toCurrency: new FormControl('', [Validators.required,
+    ]),
+  });
+
+  ngOnInit(): void {
+  }
+
+  getSymbols() {
+    this.apiService.getSymbols().pipe(takeUntil(this.ngUnSubscribe)).subscribe(
+      (res:any) => {
+        let symbolsJson = Object.keys(res?.symbols);
+        this.symbols = symbolsJson;
+
+       },
+
+    );
+  }
+
+  convertCurrency() {
+    if (this.conversionForm.valid) {
+      let storeAmount = this.conversionForm.get('amount')?.value;
+      let fromCurrency = this.conversionForm.get('fromCurrency')?.value;
+      let toCurrency = this.conversionForm.get('toCurrency')?.value;
+      this.amount = storeAmount
+      this.apiService
+        .convertCurrency(fromCurrency, toCurrency, storeAmount)
+        .pipe(takeUntil(this.ngUnSubscribe))
+        .subscribe({
+          next: (res: any) => {
+            this.conversion_result = res.conversion_result;
+            this.conversion_rate = res.conversion_rate;
+            this.base_code = res.base_code;
+            this.target_code = res.target_code;
+          },
+          complete: () =>
+            this.apiService.mostPopularConverter(fromCurrency, storeAmount),
+        });
+    }
+  }
+}
