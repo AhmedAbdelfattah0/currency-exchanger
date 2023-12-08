b991558e2ca354b851d3f49deda1c2315dedf844
AhmedAbdelfattah0 <ahmed.m.abdelfttah@gmail.com>
2023-12-08 12:00:50 +0200

    refactor: Add routing for home and details pages
    This refactor implements routing for the home and details pages in the
    Angular application. Now, users can navigate to the '/home' and
    '/details' paths, which respectively display the HomeComponent and
    DetailsComponent. Additionally, the root path redirects to '/home' and
    any unrecognized paths redirect to '/home' as well. This refactor
    improves the user experience by providing clear and intuitive navigation
    within the application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/pages/home/home.component';
+import { DetailsComponent } from './features/pages/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'details', component: DetailsComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
