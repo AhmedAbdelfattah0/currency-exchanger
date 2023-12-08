77e1b98b4ce32e513c696ce25e16afce2a292871
AhmedAbdelfattah0 <ahmed.m.abdelfttah@gmail.com>
2023-12-08 09:25:01 +0200

    Update the environment variables for API endpoints and authentication.
    Update the environment configuration to include new API endpoints and
    authentication variables for the currency conversion and charting
    functionalities. This change allows the application to interact with the
    appropriate APIs and ensures accurate and secure data retrieval.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  apiUrl: "http://data.fixer.io/api",
+  apiConvertUrl: "https://v6.exchangerate-api.com/v6/1f888faf398c63ecab7a436d/pair/",
+  apiChartUrl: "https://v6.exchangerate-api.com/v6/1f888faf398c63ecab7a436d/history/",
+  apiKey: "a0774303cc149f98c2d51a33a6727ec5",
+  base: "&base=EUR",
 };
 
 /*
